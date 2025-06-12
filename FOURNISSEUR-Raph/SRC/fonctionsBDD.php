d10024e6df43ee702e22a6340be7514b611f3f72
7ShIkI3 <raphaelecole73400@gmail.com>
2025-06-12 09:47:37 +0200

    test final

--- FOURNISSEUR-Raph/SRC/fonctionsBDD.php
+++ FOURNISSEUR-Raph/SRC/fonctionsBDD.php
@@ -1,14 +1,12 @@
 <?php
 
 function enregistreClient($clientName, $clientFirstname, $accountEmail, $accountPhone, $accountPassword, $defaultAddress, $connex) {
-    // Hachage sécurisé du mot de passe avec un fonction deja intégrer a PHP
     //$hashedPassword = password_hash($accountPassword, PASSWORD_ARGON2ID);
     $hashedPassword = $accountPassword;
 
-
-    $sql = "INSERT INTO clients (first_name, last_name, account_email, account_phone_number, account_password_hash, default_address) VALUES (:clientName, :clientFirstname, :accountEmail, :accountPhone, :accountPassword, :defaultAddress) RETURNING id";
+    $sql = "INSERT INTO clients (first_name, last_name, account_email, account_phone_number, account_password_hash, default_address) VALUES (:first_name, :last_name, :account_email, :account_phone_number, :account_password_hash, :default_address) RETURNING id";
     $res = $connex->prepare($sql);
-    
+
     $data = [
         ':first_name' => $clientFirstname,         // prénom
         ':last_name' => $clientName,              // nom de famille
@@ -17,17 +15,15 @@ function enregistreClient($clientName, $clientFirstname, $accountEmail, $account
         ':account_password_hash' => $hashedPassword,
         ':default_address' => $defaultAddress
     ];
-    
+
     $res->execute($data);
     $idClient = $res->fetchColumn();
     return $idClient;
 }
 
 function enregistreMagasinOwner($clientName, $clientFirstname, $accountEmail, $accountPhone, $accountPassword, $defaultAddress, $connex) {
-    // Hachage sécurisé du mot de passe avec une fonction déjà intégrée à PHP
     //$hashedPassword = password_hash($accountPassword, PASSWORD_ARGON2ID);
     $hashedPassword = $accountPassword;
-
 
     $sql = "INSERT INTO shop_owners (first_name, last_name, account_email, account_phone_number, account_password_hash, default_address) VALUES (:first_name, :last_name, :account_email, :account_phone_number, :account_password_hash, :default_address) RETURNING id";
     $res = $connex->prepare($sql);
