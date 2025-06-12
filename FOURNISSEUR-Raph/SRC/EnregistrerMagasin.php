61b72920b0c1bbc3ab55f96e7fdf4b19cfca386b
7ShIkI3 <raphaelecole73400@gmail.com>
2025-06-12 09:22:31 +0200

    File Corretion

--- FOURNISSEUR-Raph/SRC/EnregistrerMagasin.php
+++ FOURNISSEUR-Raph/SRC/EnregistrerMagasin.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'fonctionsConnexion.php';
 require_once 'fonctionsBDD.php';
-require_once '../../../DATA/DATABASE/CONFIG/config.php';
+require_once '../../DATA/DATABASE/CONFIG/config.php';
 
-$conn=connexionBDD('../../../DATA/DATABASE/CONFIG/config.php');
+$conn=connexionBDD('../../DATA/DATABASE/CONFIG/config.php');
 
 
 ?>
